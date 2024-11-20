b5767f539360da93f4189c5388eecb012185c962
Egor1198 <belosokhovegor@gmail.com>
2024-11-20 22:24:40 +0300

    module_10_5

--- module_10_5.py
+++ module_10_5.py
@@ -1,28 +1,30 @@
-import time
-from multiprocessing import Pool
+import datetime
+import multiprocessing
+
 
 def read_info(name):
     all_data = []
-    with open(name, 'r') as file:
+    with open(name) as file:
         while True:
             line = file.readline()
+            all_data.append(line)
             if not line:
                 break
-            all_data.append(line.strip())
-    return all_data
+
 
 filenames = [f'./file_{number}.txt' for number in range(1, 5)]
 
 
-if __name__ == '__main__':
-    start_time = time.time()
-    for filename in filenames:
-        read_info(filename)
-    end_time = time.time()
-    print(f"Линейный вызов: {end_time - start_time:.3f} секунд")
+start_1 = datetime.datetime.now()
+for file in filenames:
+    read_info(file)
+end_1 = datetime.datetime.now()
+print(f'{end_1 - start_1} (линейный)')
 
-    start_time = time.time()
-    with Pool() as pool:
+
+if __name__ == '__main__':
+    start_2 = datetime.datetime.now()
+    with multiprocessing.Pool(processes=len(filenames)) as pool:
         pool.map(read_info, filenames)
-    end_time = time.time()
-    print(f"Многопроцессный вызов: {end_time - start_time:.3f} секунд")
+    end_2 = datetime.datetime.now()
+    print(f'{end_2 - start_1} (многопроцессорный)')
